0071d6dce169eb004723899bb2fc2f4224415a6d
Mark Hesketh <contact@markhesketh.co.uk>
2025-05-07 14:07:19 +0100

    Simplify render call in controller

--- app/controllers/opengraph_image_controller.rb
+++ app/controllers/opengraph_image_controller.rb
@@ -1,9 +1,7 @@
 class OpengraphImageController < ApplicationController
   def show
     @post = Post.find(params[:post_id])
-    respond_to do |format|
-      format.png { render plain: @post.opengraph_image.to_png, content_type: "image/png", disposition: "inline" }
-    end
+    render plain: @post.opengraph_image.to_png, content_type: "image/png", disposition: "inline"
     expires_in 3.hours, public: true
   end
 end
